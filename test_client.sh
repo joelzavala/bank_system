593db029487fdff16902164f0d60b1868a66041b
Joel Zavala <joel.zavala.duran@gmail.com>
2024-08-04 22:04:04 -0600

    client script fixed

--- test_client.sh
+++ test_client.sh
@@ -1,4 +1,3 @@
 for ((i = 0 ; i < 10 ; i++)); do
-  curl -X POST "http://localhost:8080/retirar.php" -d amount=$((i + 1) * 10) -H "Content-Type: application/x-www-form-urlencoded" &
+  curl -X POST "http://localhost/withdraw.php" #-d amount=$((i + 1) * 10) -H "Content-Type: application/x-www-form-urlencoded" &
 done
-
